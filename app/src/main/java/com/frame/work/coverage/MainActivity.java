b478b4b1eca17befcdd2b7d541b20a1fa1923862
Mimo <mimosaha@gmail.com>
2019-01-01 16:09:30 +0600

    add framework library

--- app/src/main/java/com/frame/work/coverage/MainActivity.java
+++ app/src/main/java/com/frame/work/coverage/MainActivity.java
@@ -3,13 +3,17 @@ package com.frame.work.coverage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import core.left.io.framework.application.ui.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
 
+    @Override
+    protected void startUI() {
 
     }
 }
